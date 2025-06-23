4114112d7e7f1debb8e002af1e5af9533a38edc0
SergiyOl <ser4002@gmail.com>
2025-06-23 13:47:32 +0300

    Created songDTO and list for songs

--- DotNet Rest API/Program.cs
+++ DotNet Rest API/Program.cs
@@ -1,5 +1,31 @@
+using DotNet_Rest_API.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<SongDto> songs = [
+    new (
+        1,
+        "song1",
+        "Rock",
+        186,
+        1308
+        ),
+    new (
+        2,
+        "song2",
+        "Indie",
+        168,
+        2231
+        ),
+    new (
+        3,
+        "song3",
+        "Pop",
+        191,
+        6890
+        )
+];
 
 app.MapGet("/", () => "Hello World!");
 
