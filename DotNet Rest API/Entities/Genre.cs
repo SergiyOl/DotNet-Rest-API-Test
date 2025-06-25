71b8202871e2ec49b3d019b3fbc5e4ce53f8fc18
SergiyOl <ser4002@gmail.com>
2025-06-25 14:26:07 +0300

    Created entities

---
+++ DotNet Rest API/Entities/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet_Rest_API.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ DotNet Rest API/Entities/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNet_Rest_API.Entities
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; }
+        public int Lenght { get; set; }
+        public int Listens { get; set; }
+
+    }
+}
