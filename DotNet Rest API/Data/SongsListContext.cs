244475e876a597cb1f061444ee41c82a9c354abb
SergiyOl <ser4002@gmail.com>
2025-06-25 15:12:56 +0300

    Created DB Context

---
+++ DotNet Rest API/Data/SongsListContext.cs
@@ -0,0 +1,12 @@
+﻿using DotNet_Rest_API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.CodeDom.Compiler;
+
+namespace DotNet_Rest_API.Data
+{
+    public class SongsListContext(DbContextOptions<SongsListContext> options) : DbContext(options)
+    {
+        public DbSet<Song> Songs => Set<Song>();
+        public DbSet<Genre> Genres => Set<Genre>();
+    }
+}
--- DotNet Rest API/Program.cs
+++ DotNet Rest API/Program.cs
@@ -1,7 +1,12 @@
-using DotNet_Rest_API.DTOs;
+using DotNet_Rest_API.Data;
 using DotNet_Rest_API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//SQLite
+var connString = builder.Configuration.GetConnectionString("SongsList");
+builder.Services.AddSqlite<SongsListContext>(connString);
+
 var app = builder.Build();
 app.MapSongsEndpoints();
 
