a5a69c3f136228af82c674091fcf8217a80f701a
SergiyOl <ser4002@gmail.com>
2025-06-25 13:40:20 +0300

    Moved endpoins to an extention class.

---
+++ DotNet Rest API/Endpoints/SongsEndpoints.cs
@@ -0,0 +1,93 @@
+﻿using DotNet_Rest_API.DTOs;
+
+namespace DotNet_Rest_API.Endpoints
+{
+    public static class SongsEndpoints
+    {
+        private static readonly List<SongDto> songs = [
+            new (
+                0,
+                "song1",
+                "Rock",
+                186,
+                1308
+                ),
+            new (
+                1,
+                "song2",
+                "Indie",
+                168,
+                2231
+                ),
+            new (
+                2,
+                "song3",
+                "Pop",
+                191,
+                6890
+                )
+        ];
+
+        public static WebApplication MapSongsEndpoints(this WebApplication app)
+        {
+            // GET /songs
+            app.MapGet("songs", () => songs);
+
+            // GET /songs/(id)
+            app.MapGet("songs/{id}", (int id) =>
+            {
+                SongDto? song = songs.Find(song => song.Id == id);
+
+                return song is null ? Results.NotFound() : Results.Ok(song);
+            })
+            .WithName("GetSong");
+
+            // POST /songs
+            app.MapPost("songs", (CreateSongDto newSong) =>
+            {
+                SongDto song = new(
+                    songs.Count,
+                    newSong.Name,
+                    newSong.Genre,
+                    newSong.Lenght,
+                    newSong.Listens);
+
+                songs.Add(song);
+
+                return Results.CreatedAtRoute("GetSong", new { id = song.Id }, song);
+            });
+
+            // PUT /songs/(id)
+            app.MapPut("songs/{id}", (int id, UpdateSongDto updatedSong) =>
+            //songs.Find(song => song.Id == id)).WithName("GetSong");
+            {
+                var index = songs.FindIndex(song => song.Id == id);
+
+                if (index == -1)
+                {
+                    return Results.NotFound();
+                }
+
+                songs[index] = new SongDto(
+                    id,
+                    updatedSong.Name,
+                    updatedSong.Genre,
+                    updatedSong.Lenght,
+                    updatedSong.Listens
+                );
+
+                return Results.NoContent();
+            });
+
+            // DELETE /songs/(id)
+            app.MapDelete("songs/{id}", (int id) =>
+            {
+                songs.RemoveAll(song => song.Id == id);
+
+                return Results.NoContent();
+            });
+
+            return app;
+        }
+    }
+}
--- DotNet Rest API/Program.cs
+++ DotNet Rest API/Program.cs
@@ -1,90 +1,9 @@
 using DotNet_Rest_API.DTOs;
+using DotNet_Rest_API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-List<SongDto> songs = [
-    new (
-        0,
-        "song1",
-        "Rock",
-        186,
-        1308
-        ),
-    new (
-        1,
-        "song2",
-        "Indie",
-        168,
-        2231
-        ),
-    new (
-        2,
-        "song3",
-        "Pop",
-        191,
-        6890
-        )
-];
-
-// GET /songs
-app.MapGet("songs", () => songs);
-
-// GET /songs/(id)
-app.MapGet("songs/{id}", (int id) =>
-{
-    SongDto? song = songs.Find(song => song.Id == id);
-
-    return song is null ? Results.NotFound() : Results.Ok(song);
-})
-.WithName("GetSong");
-
-// POST /songs
-app.MapPost("songs", (CreateSongDto newSong) =>
-{
-    SongDto song = new(
-        songs.Count,
-        newSong.Name,
-        newSong.Genre,
-        newSong.Lenght,
-        newSong.Listens);
-
-    songs.Add(song);
-
-    return Results.CreatedAtRoute("GetSong", new { id = song.Id }, song);
-});
-
-// PUT /songs/(id)
-app.MapPut("songs/{id}", (int id, UpdateSongDto updatedSong) =>
-//songs.Find(song => song.Id == id)).WithName("GetSong");
-{
-    var index = songs.FindIndex(song => song.Id == id);
-
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    songs[index] = new SongDto(
-        id,
-        updatedSong.Name,
-        updatedSong.Genre,
-        updatedSong.Lenght,
-        updatedSong.Listens
-    );
-
-    return Results.NoContent();
-});
-
-// DELETE /songs/(id)
-app.MapDelete("songs/{id}", (int id) =>
-{
-    songs.RemoveAll(song => song.Id == id);
-
-    return Results.NoContent();
-});
-
-
+app.MapSongsEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 app.Run();
