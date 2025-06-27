35093987d9b5fb7bed10ecf8804d70b55d72d039
SergiyOl <ser4002@gmail.com>
2025-06-27 15:38:32 +0300

    Added default genres in database

--- DotNet Rest API/Data/SongsListContext.cs
+++ DotNet Rest API/Data/SongsListContext.cs
@@ -8,5 +8,16 @@ namespace DotNet_Rest_API.Data
     {
         public DbSet<Song> Songs => Set<Song>();
         public DbSet<Genre> Genres => Set<Genre>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new { Id = 1, Name = "Pop" },
+                new { Id = 2, Name = "Rock" },
+                new { Id = 3, Name = "Metal" },
+                new { Id = 4, Name = "Indie" },
+                new { Id = 5, Name = "Rap" }
+            );
+        }
     }
 }
