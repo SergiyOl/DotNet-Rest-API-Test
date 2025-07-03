c5b416db5fdccf6ed2f33da4619e71459d162640
SergiyOl <ser4002@gmail.com>
2025-07-03 14:34:16 +0300

    Added Identity builder to Program.cs

--- DotNet Rest API/Program.cs
+++ DotNet Rest API/Program.cs
@@ -1,5 +1,7 @@
 using DotNet_Rest_API.Data;
 using DotNet_Rest_API.Endpoints;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("SongsList");
 builder.Services.AddSqlite<AppDBContext>(connString);
 
+//Identities
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => 
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDBContext>();
+
+//App build
 var app = builder.Build();
+//Endpoints
 app.MapSongsEndpoints();
 app.MapGenresEndpoints();
-
+//Migration
 await app.MigrateDBAsync();
 
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//App start
 app.MapGet("/", () => "Hello World!");
 app.Run();
