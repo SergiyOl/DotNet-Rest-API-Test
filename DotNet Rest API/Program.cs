d3102082530ce357178cf4cc5ca5c3cc64eaf186
SergiyOl <ser4002@gmail.com>
2025-07-07 15:16:16 +0300

    Revert "Added Identity builder to Program.cs"
    This reverts commit c5b416db5fdccf6ed2f33da4619e71459d162640.

--- DotNet Rest API/Program.cs
+++ DotNet Rest API/Program.cs
@@ -1,7 +1,5 @@
 using DotNet_Rest_API.Data;
 using DotNet_Rest_API.Endpoints;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,23 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("SongsList");
 builder.Services.AddSqlite<AppDBContext>(connString);
 
-//Identities
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => 
-    options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDBContext>();
-
-//App build
 var app = builder.Build();
-//Endpoints
 app.MapSongsEndpoints();
 app.MapGenresEndpoints();
-//Migration
+
 await app.MigrateDBAsync();
 
-//Auth
-app.UseAuthentication();
-app.UseAuthorization();
-
-//App start
 app.MapGet("/", () => "Hello World!");
 app.Run();
